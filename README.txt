0282084ee94955ecce7a0d6e470f031b7ad58b92
Steven A. Zahm <shazahm1@hotmail.com>
2018-07-23 13:30:18 -0400

    Bump version to 1.0.2.

--- README.txt
+++ README.txt
@@ -5,7 +5,7 @@ Tags: business directory, anniversary, birthday, email
 Requires at least: 4.5.3
 Tested up to: 4.9
 Requires PHP: 5.3
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- connections-business-directory-anniversary-and-birthday-emails.php
+++ connections-business-directory-anniversary-and-birthday-emails.php
@@ -13,7 +13,7 @@
  * Plugin Name:       Connections Business Directory Anniversary and Birthday Emails
  * Plugin URI:        https://connections-pro.com/documentation/anniversary-and-birthday-emails/
  * Description:       An extension for the Connections Business Directory plugin which adds the ability to automatically send an email to entries on their anniversary or birthday.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Steven A. Zahm
  * Author URI:        http://connections-pro.com
  * License:           GPL-2.0+
@@ -31,7 +31,7 @@ if ( ! class_exists( 'Connections_Anniversary_and_Birthday_Emails' ) ) :
 
 	final class Connections_Anniversary_and_Birthday_Emails {
 
-		const VERSION = '1.0.1';
+		const VERSION = '1.0.2';
 		const LOG     = FALSE;
 
 		/**
